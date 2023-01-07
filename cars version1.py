e09fcae24f017994757302707350eb1c4ebb67ee
DEVELoper <syabblogger@gmail.com>
2023-01-07 14:01:39 +0500

    Linear Regression Example Covered

--- cars version1.py
+++ cars version1.py
@@ -3,16 +3,19 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 data = pd.read_csv("cars.csv", header=None)
 
-cars1T = data[:-20]
-cars2T = data[-20:]
+trimedData = data[:20]
+print(trimedData)
 
-cars3T = data[:-20]
-cars4T = data[-20:]
-
-linear = LinearRegression()
-linear.fit(cars1T, cars2T)
-cars4predection = linear.predict(cars4T)
-plt.plot(cars4predection)
-plt.show()
+# cars1T = data[:-20]
+# cars2T = data[-20:]
+#
+# cars3T = data[:-20]
+# cars4T = data[-20:]
+#
+# linear = LinearRegression()
+# linear.fit(cars1T, cars2T)
+# cars4predection = linear.predict(cars4T)
+# plt.plot(cars4predection)
+# plt.show()
 
 # print(linear.predict(cars4T))
