908276e10e995e478d40baa613d5f9c4add9cc1d
DEVELoper <syabblogger@gmail.com>
2023-01-08 01:18:34 +0500

    joblib and creating module

--- music algorithm 2.py
+++ music algorithm 2.py
@@ -2,17 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-musicData = pd.read_csv("music.csv")
+import joblib
 
-X = musicData.drop(columns=['genre'])
-Y = musicData[['genre']]
+# musicData = pd.read_csv("music.csv")
+# X = musicData.drop(columns=['genre'])
+# Y = musicData[['genre']]
+#
+# Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2)
+# model = DecisionTreeClassifier()
+# mod = model.fit(Xtrain, Ytrain)
+# joblib.dump(mod,"musicfile.joblib")
+model = joblib.load("musicfile.joblib")
+predection = model.predict([[21,1],[34,0]])
+print(predection)
 
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2)
 
-model = DecisionTreeClassifier()
-model.fit(Xtrain, Ytrain)
-predictions = model.predict(Xtest)
-score = accuracy_score(Ytest, predictions)
-print(score)
+# predictions = model.predict(Xtest)
+# score = accuracy_score(Ytest, predictions)
+# print(score)
 
